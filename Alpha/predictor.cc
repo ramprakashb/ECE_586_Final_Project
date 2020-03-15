21a45a3f769a781cf2fbfcaf20f87f7c5fc81161
michaelescue <mescue@pdx.edu>
2020-03-15 15:04:37 -0700

    Tournament Gshare Predictor

--- Alpha/predictor.cc
+++ Alpha/predictor.cc
@@ -9,17 +9,18 @@
 #include "predictor.h"
 
 /*Variables- gshare*/
-#define BHMASK 0xFFF			//10 bits for branch history
-#define BAMASK 0xFFF			//10 bits for branch Address
-#define PREDICT_SIZE 4*1024		//10 bits for Predictor Table Size
+#define BRANCH_HIST_MASK 0x1FF			//10 bits for branch history
+#define BAMASK 0x1FF					//10 bits for branch Address
+#define PREDICT_SIZE 512				//10 bits for Predictor Table Size
 #define SETLSB 0x1
-#define PATHHISTVAL 0xFFF
 //#define BS_VERBOSE
 
 static unsigned int xor_output = 0;
 static unsigned int branch_address = 0;
 static unsigned int path_history = 0;
 static unsigned int predict_table_g[PREDICT_SIZE] = {0};
+static unsigned int predict_table_l[PREDICT_SIZE] = {0};
+static unsigned int choice_table[PREDICT_SIZE] = {0};
 
 /*function Prototypes- gshare*/
 unsigned int xor_comb(unsigned int a, unsigned int b);
@@ -27,19 +28,23 @@ bool get_predict(void);				// returs prediction
 void update_global_branch_history(bool);				// updates path history
 void update_predict_table(bool);						// updates predictor pointed to by hash
 void get_branch_address(const branch_record_c* br);
+void update_predict_table_l(bool taken);
+void update_predict_table_g(bool taken);
+void update_choice_table(bool taken);
 void debug(unsigned int val, const char *tag);			// prints values in column format
-
 
 
 bool PREDICTOR::get_prediction(const branch_record_c* br  , const op_state_c* os ){
 	//Gshare
+	//if(!(br->is_conditional)) return 1;
+
 	//Get address branch
 	get_branch_address(br);
 
-	//xor
+	/*	Hash the branch history and instruction address	*/
 	xor_output = xor_comb(path_history, branch_address);
 
-	//Get Prediction table values
+	/*	Make prediction	*/
 	return get_predict();
 
 }
@@ -53,61 +58,85 @@ void get_branch_address(const branch_record_c* br)
 
 unsigned int xor_comb(unsigned int a, unsigned int b)
 {
-	return  (BHMASK & (a ^ b));
+	return  (a ^ b);
 }
+
 bool get_predict(void)
 {
 	/*	Debug	*/ 	debug(xor_output, "xor_output");
+	/*	Debug	*/	debug(predict_table_l[xor_output], "predict_table_l[xor]");
 	/*	Debug	*/	debug(predict_table_g[xor_output], "predict_table_g[xor]");
+	/*	Debug	*/	debug(choice_table[xor_output], "choice_table[xor]");
 
-    if ((predict_table_g[xor_output] >> 1) & 0x1)	// Return value based on MSb.
-        return true;
-	else
-        return false;
+	if		(0x1 & (choice_table[xor_output] >> 1))
+	return	((predict_table_g[xor_output] >> 1) & 0x1) ? true : false;
+	else 
+	return	((predict_table_l[xor_output] >> 1) & 0x1) ? true : false;
     
 }
-    
+
+ /********************************************************************************************************/  
+
 // Update the predictor after a prediction has been made.  This should accept
 // the branch record (br) and architectural state (os), as well as a third
 // argument (taken) indicating whether or not the branch was taken.
 void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken){
+	/*	Update tables	*/
+	update_predict_table_l(taken);
+	update_predict_table_g(taken);
+	update_choice_table(taken);
 
 	//Update path history values
 	update_global_branch_history(taken);
-
-	//update predict table
-	update_predict_table(taken);
-
+	
 	/*	Debug	*/	debug(0, "NEWLINE");
 
 }
 
 void update_global_branch_history(bool taken)
 {
-	if(taken)
-		path_history = PATHHISTVAL & ((path_history << 1) | SETLSB);
-	else
-		path_history = PATHHISTVAL & (path_history << 1);
-	
+	if(taken) path_history = BRANCH_HIST_MASK & ((path_history << 1) | SETLSB);
+	else path_history = BRANCH_HIST_MASK & (path_history << 1);
 	/*	Debug	*/	debug(path_history, "path_history");
 
 }
 
-void update_predict_table(bool taken)
+void update_predict_table_l(bool taken)
 {
 		if(taken)
         {
-			if(predict_table_g[xor_output] < 0x3)
-			 	predict_table_g[xor_output]++;
+			if(predict_table_l[xor_output] < 0x3) predict_table_l[xor_output]++;
         }
-		else if(predict_table_g[xor_output] > 0x0)
-        {
-				predict_table_g[xor_output]--;
-        }
+		else if(predict_table_l[xor_output] > 0x0) predict_table_l[xor_output]--;
 
 		/*	Debug	*/	debug(predict_table_g[xor_output], "Predict_table_g[xor]-U");
 }
 
+void update_predict_table_g(bool taken)
+{
+		if(taken)
+        {
+			if(predict_table_g[xor_output] < 0x3) predict_table_g[xor_output]++;
+        }
+		else if(predict_table_g[xor_output] > 0x0) predict_table_g[xor_output]--;
+
+		/*	Debug	*/	debug(predict_table_g[xor_output], "Predict_table_g[xor]-U");
+}
+
+void update_choice_table(bool taken)
+{
+	if (((predict_table_g[xor_output] >> 1) & 0x1) == taken) {					// ?/?
+		if (!(((predict_table_l[xor_output] >> 1) & 0x1) == taken)) {				// 1/?
+			if (choice_table[xor_output] < 0x3)		// 1/0
+				choice_table[xor_output]++;
+		}
+	}
+	else if (((predict_table_l[xor_output] >> 1) & 0x1) == taken) { 				// 0/?
+		if (choice_table[xor_output] > 0x0)		// 0/1
+			choice_table[xor_output]--;
+	}
+	/*	Debug	*/	debug(predict_table_g[xor_output], "Predict_table_g[xor]-U");
+}
 void debug(unsigned int val, const char *tag){
 	#ifdef	BS_VERBOSE
 	FILE * fh = NULL;
